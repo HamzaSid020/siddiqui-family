334bad5b6a7190d111dcafe7e954b02be8701e0a
Hamza Siddiqui <93541645+HamzaSid020@users.noreply.github.com>
2025-02-14 03:03:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,38 +6,21 @@ async function fetchData() {
 async function createFamilyTree() {
     const data = await fetchData();
 
-    const f3Chart = f3.createChart('#FamilyChart', data)
+const f3Chart = f3.createChart('#FamilyChart', data)
         .setTransitionTime(1000)
         .setCardXSpacing(250)
         .setCardYSpacing(150)
         .setOrientationVertical()
-        .setSingleParentEmptyCard(true, { label: 'ADD' })
-
-    const f3Card = f3Chart.setCard(f3.CardHtml)
-        .setCardDisplay([["first name", "last name"], ["birthday"]])
-        .setCardDim({})
+        .setSingleParentEmptyCard(true, {label: 'ADD'})
+    
+      const f3Card = f3Chart.setCard(f3.CardHtml)
+        .setCardDisplay([["first name","last name"],["birthday"]])
+        .setCardDim({"width":215,"height":75,"img_x":7})
         .setMiniTree(true)
-        .setStyle('imageCircle')
+        .setStyle('imageRect')
         .setOnHoverPathToMain()
 
-
-    const f3EditTree = f3Chart.editTree()
-        .fixed(true)
-        .setFields(["first name", "last name", "birthday", "avatar"])
-        .setEditFirst(false)
-
-    f3EditTree.setEdit()
-
-    f3Card.setOnCardClick((e, d) => {
-        f3EditTree.open(d)
-        if (f3EditTree.isAddingRelative()) return
-        f3Card.onCardClickDefault(e, d)
-    });
-
-    f3Chart.updateTree({ initial: true })
-    f3EditTree.open(f3Chart.getMainDatum())
-
-    f3Chart.updateTree({ initial: true })
+      f3Chart.updateTree({initial: true})
 }
 
 // Initialize the family tree
